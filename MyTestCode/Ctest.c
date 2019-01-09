6690eb4db1a4eff26803226f27ddca8c57601e03
zhaoxinju <zhaoxinju2076@126.com>
2019-01-09 16:49:31 +0800

    恢复

--- MyTestCode/Ctest.c
+++ MyTestCode/Ctest.c
@@ -12,11 +12,7 @@ int sumtest(void)
 {
     if(x > y)
     {
-<<<<<<< HEAD
         return y * y;
-=======
-        return x * x;
->>>>>>> 4b94387a6405301b27a12b6704e5b7428e3d7154
     }
     else
     {
